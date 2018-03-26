c0d4f9235e83f1f3bc91e2d8dce3f30b6f9ef0b1
ramius84 <hiddenworks11@gmail.com>
2018-03-27 00:05:06 +0200

    Uploaded project
    Uploaded project & executable files

---
+++ UDPServerConnect/UDPServerConnect/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPServerConnect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int port = (int)numericUpDown1.Value;
+            IPEndPoint remoteIP = new IPEndPoint(IPAddress.Any, 0);
+            try
+            {
+                UdpClient server = new UdpClient(port);
+                Byte[] read = server.Receive(ref remoteIP);
+                string data = Encoding.ASCII.GetString(read);
+                listBox1.Items.Add(data);
+                server.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+    }
+}
